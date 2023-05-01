16444c73dace8794c7d447f67a215d8c8c64218e
alekmin1234@gmail.com <alekmin1234@gmail.com>
2023-05-01 19:00:17 +0200

    Zadanie ,dzień 4.

--- stat/stat/Program.cs
+++ stat/stat/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using System.ComponentModel.Design;
+
+var name ="Alek";
+var gender = "Male";
+var age = 20;
+
+if (gender =="Male")
+{
+    if (age < 30)
+    {
+        Console.WriteLine("Mężczyzna poniżej 30 lat");  
+    } 
+    else if (age == 20 && name == "Alek")
+    {
+        Console.WriteLine("Alek, lat 20");
+    }
+}
+if (gender == "Male")
+{
+    if (age < 18)
+    {
+        Console.WriteLine("Niepełnoletni Mężczyzna");
+    }
+    else if (age >= 18)
+    {
+        Console.WriteLine("Pełnoletni Mężczyzna");
+    }
+}
+
+
+
+    
+    
